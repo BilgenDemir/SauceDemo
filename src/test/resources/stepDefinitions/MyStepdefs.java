80640b3fe85a6ef196b1c319fa60c4d0200f93d8
BilgenDemir <bilgenbersu86@gmail.com>
2023-12-05 02:32:53 +0300

    first commit for SauceDemo project

---
+++ src/test/resources/stepDefinitions/MyStepdefs.java
@@ -0,0 +1,9 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+    @Then("Check {string}message about username")
+    public void checkMessageAboutUsername(String arg0) {
+    }
+}
